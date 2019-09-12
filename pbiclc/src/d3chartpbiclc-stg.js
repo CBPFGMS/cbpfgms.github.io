6a830768f8cb7da6626c593b3e81b621839434d0
Gerardo Furtado <gerardofurtado@gmail.com>
2019-09-12 20:07:18 +1000

    Update d3chartpbiclc-stg.js

--- pbiclc/src/d3chartpbiclc-stg.js
+++ pbiclc/src/d3chartpbiclc-stg.js
@@ -2,6 +2,7 @@
 
 	const isInternetExplorer = window.navigator.userAgent.indexOf("MSIE") > -1 || window.navigator.userAgent.indexOf("Trident") > -1,
 		hasFetch = window.fetch,
+		hasURLSearchParams = window.URLSearchParams,
 		isTouchScreenOnly = (window.matchMedia("(pointer: coarse)").matches && !window.matchMedia("(any-pointer: fine)").matches),
 		isPfbiSite = window.location.hostname === "pfbi.unocha.org",
 		isBookmarkPage = window.location.hostname + window.location.pathname === "bi-home.gitlab.io/CBPF-BI-Homepage/bookmark.html",
@@ -30,6 +31,10 @@
 	if (!isScriptLoaded(d3URL)) {
 		if (hasFetch) {
 			loadScript(d3URL, d3Chart);
+		} else if (hasFetch && !hasURLSearchParams) {
+			loadScript("https://cdn.jsdelivr.net/npm/@ungap/url-search-params@0.1.2/min.min.js", function() {
+				loadScript(d3URL, d3Chart);
+			});
 		} else {
 			loadScript("https://cdn.jsdelivr.net/npm/promise-polyfill@7/dist/polyfill.min.js", function() {
 				loadScript("https://cdnjs.cloudflare.com/ajax/libs/fetch/2.0.4/fetch.min.js", function() {
