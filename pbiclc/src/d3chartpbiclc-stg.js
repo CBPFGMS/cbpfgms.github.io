cc6c846bf29404080173ebc94fbe5013ba5e7a9e
Gerardo Furtado <gerardofurtado@gmail.com>
2023-01-09 22:45:38 +1000

    Update d3chartpbiclc-stg.js

--- pbiclc/src/d3chartpbiclc-stg.js
+++ pbiclc/src/d3chartpbiclc-stg.js
@@ -746,7 +746,7 @@
 			} else {
 				d3.select(window).on("scroll.pbiclc", checkPosition);
 				d3.select("body").on("d3ChartsYear.pbiclc", function() {
-					chartState.selectedYear = [validateCustomEventYear(+d3.event.detail)]
+					chartState.selectedYear = [validateCustomEventYear(+d3.event.detail)];
 					console.log(chartState.selectedYear);
 					if (chartState.selectedYear > currentYear) chartState.selectedContribution = "total";
 				});
