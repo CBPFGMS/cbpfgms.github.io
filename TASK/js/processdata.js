66b96159644daa7e6c722d510463ca44a0d88cda
Gerardo Furtado <gerardofurtado@gmail.com>
2023-06-07 16:12:50 +1000

    Several filters for nodes and links

--- TASK/js/processdata.js
+++ TASK/js/processdata.js
@@ -46,22 +46,48 @@ function processData(rawData) {
 				data.links.push({
 					source: node.StatusId,
 					target: link.NextStatusId,
-					text: "",
 					type: link.LinkType,
-					id: ++counterLink,
 					isCompleted: link.IsCompleted,
 					tasks: link.Tasks,
+					text: link.Tasks[0].TaskName, //This should be changed to filter the task name accordingly
+					show: true,
 				});
 			}
 		});
 	});
 
-	data.links.forEach(link => {
-		//This should be changed to filter the task name accordingly
-		link.text = link.tasks[0].TaskName;
-	});
+	const nodesAlreadyVisited = new Set();
+
+	reachableNode(data.currentStatus, true);
+
+	data.links = data.links.filter(e => e.show);
+
+	//eliminates nodes that are not connected to any link
+	data.nodes = data.nodes.filter(e =>
+		data.links.find(f => f.source === e.id || f.target === e.id)
+	);
+
+	data.links.forEach(link => (link.id = ++counterLink));
 
 	return data;
+
+	function reachableNode(currentNode, reachable) {
+		nodesAlreadyVisited.add(currentNode);
+		const previousLinks = data.links.filter(e => e.target === currentNode);
+		previousLinks.forEach(link => {
+			const node = link.source;
+			const linkToPrevious = data.links.find(
+				e => e.source === currentNode && e.target === node
+			);
+			if (!linkToPrevious || !reachable) {
+				data.links
+					.filter(e => e.source === node && !e.isCompleted)
+					.forEach(e => (e.show = false));
+			}
+			if (!nodesAlreadyVisited.has(link.source))
+				reachableNode(node, reachable && !!linkToPrevious);
+		});
+	}
 }
 
 export { processData };
