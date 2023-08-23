2939ec45349298fe0d300462e41540f3baf695b6
Gerardo Furtado <gerardofurtado@gmail.com>
2023-08-23 11:14:41 +1000

    Data files

--- sandbox/resultsdash/src/utils/api.ts
+++ sandbox/resultsdash/src/utils/api.ts
@@ -1,9 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-function useData(){
+//types
 
-	
+function useData() {
+	const byClusterUrl = "",
+		byDisabilityUrl = "",
+		byLocationUrl = "",
+		byTypeUrl = "",
+		locationMasterUrl = "";
 
+	const [data, setData] = useState(null),
+		[loading, setLoading] = useState<boolean>(true),
+		[error, setError] = useState<unknown>(null);
 }
 
 export default useData;
