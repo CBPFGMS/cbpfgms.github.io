fc203e0ff1bec4a87c8f077a7c5be54e78cb764d
Gerardo Furtado <gerardofurtado@gmail.com>
2024-09-27 14:55:10 +1000

    Data array for the API calls started

--- utils/apibench/src/main.ts
+++ utils/apibench/src/main.ts
@@ -1,5 +1,5 @@
 import { test } from "./schemas";
 
 console.log(test.parse({ name: "John" }));
-console.log("test finished with glory!!!");
+console.log("test finished with glory!!!!!");
 
--- utils/apibench/src/schemas.ts
+++ utils/apibench/src/schemas.ts
@@ -1,7 +1,33 @@
 import { z } from "zod";
 
-const test = z.object({
-	name: z.string(),
+const charts = z.enum([
+	"Allocations_overview",
+	"Results_dashboard",
+	"Progress_dashboard",
+	"CBPF_vs_HRP",
+	"CBPF_by_year",
+	"Allocation_flow",
+	"Allocations",
+	"Allocations_timeline",
+	"Contributions",
+	"Contribution_trends",
+	"Funding_overview",
+	"Sectors",
+	"Targeted_and_reached_people",
+	"Gender_with_age_marker",
+]);
+
+const apiTypes = z.enum(["data", "master"]);
+
+const datum = z.object({
+	url: z.string().url(),
+	queryString: z.string().nullable(),
+	apiName: z.string(),
+	charts: z.array(charts),
+	apiType: apiTypes,
+	date: z.date(),
+	dataReceived: z.boolean(),
+	downloadTime: z.number().nonnegative(),
 });
 
-export { test };
+export type Datum = z.infer<typeof datum>;
