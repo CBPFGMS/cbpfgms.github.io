8de1ef27a0970d02be2fb3f7822543c6e15add71
Gerardo Furtado <gerardofurtado@gmail.com>
2025-09-09 13:56:10 +1000

    Out file with fixed name

--- cbpfcva/vite.config.ts
+++ cbpfcva/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: "assets/cva.js",
+			},
+		},
+	},
+});
