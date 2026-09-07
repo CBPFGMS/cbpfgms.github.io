649653c1fb65616789021a0880d8aadf926b526c
Gerardo Furtado <gerardofurtado@gmail.com>
2026-09-07 19:45:10 +1000

    Correct cut-off date

--- contributions-fasttrack/src/utils/constants.ts
+++ contributions-fasttrack/src/utils/constants.ts
@@ -5,7 +5,7 @@ export const constants = {
 	localStorageTime: 60 * 60 * 1000, //1 hour
 	consoleStyle: "background-color: #0d6cb6; color: white; padding: 2px;",
 	isProdSite: window.location.hostname === "cbpf.data.unocha.org",
-	cutOffDate: new Date("2026-07-01"),
+	cutOffDate: new Date("2026-06-01"),
 	tranches: [1, 2, "all"],
 	contributionTypes: ["total", "paid", "pledged"],
 	minWidth: 1200,
